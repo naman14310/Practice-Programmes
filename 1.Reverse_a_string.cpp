9d063be88afa9f803dbf97c6013242204c337c48
KaranBhut44 <70577660+KaranBhut44@users.noreply.github.com>
2020-10-02 02:28:58 +0530

    Update 1.Reverse_a_string.cpp

--- 1.Reverse_a_string.cpp
+++ 1.Reverse_a_string.cpp
@@ -6,22 +6,11 @@ using namespace std;
 int main()
 {
     string s1;
-    int i=0, len=0, temp=0;
     cout<<"Enter string : ";
-
-    {
-    	len++;
-    	temp++;
-	}
-    
-    char s2[len];
-    while(i<len)
-    {
-    	s2[i]= s1[len-i-1];
-    	i++;
-	}
-    
-    cout<<s2;*/
-    
+    cin>>s1;
+    string s2="";
+    for(int i=s1.length()-1;i>=0;i--)
+        s2+=s1[i];
+    cout<<s2;
 } 
 
